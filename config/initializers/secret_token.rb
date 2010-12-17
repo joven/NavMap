053f5280d821f7b85863dff5116edf0b1a988245
Joven Larin <jslarin@apollo.com.ph>
2010-12-17 14:04:00 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NavMap::Application.config.secret_token = '4beff0244604ccba431c8f51dc71fa3ed38f14ca4dfbd552a0b8a8c06250d30d81adcba6c81f08855715bffb8e680551574b1d23bb2a7b9ad6562aee63b7a9bd'
